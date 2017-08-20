fe0b39902c5538e3e00de30b510ac0234040bb62
danielVebman <danveb800@gmail.com>
2017-08-20 16:39:21 -0400

    Create Colors.swift

---
+++ Colors.swift
@@ -0,0 +1,18 @@
+//
+//  UIStatusBarAlert.swift
+//  UIStatusBarAlert
+//
+//  Created by Daniel Vebman on 8/20/17.
+//  Copyright © 2017 Daniel Vebman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let successGreen = UIColor(colorLiteralRed: 62 / 255, green: 224 / 255, blue: 70 / 255, alpha: 1)
+    static let calmBlue = UIColor(colorLiteralRed: 66 / 255, green: 134 / 255, blue: 244 / 255, alpha: 1)
+    static let failureRed = UIColor(colorLiteralRed: 216 / 255, green: 45 / 255, blue: 45 / 255, alpha: 1)
+    static let warningYellow = UIColor(colorLiteralRed: 252 / 255, green: 231 / 255, blue: 0, alpha: 1)
+    static let cautionOrange = UIColor(colorLiteralRed: 239 / 255, green: 159 / 255, blue: 9 / 255, alpha: 1)
+}
